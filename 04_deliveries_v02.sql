8408f5adc41ea49d85f5528412143a266f6f2110
Paritosh Sharma <paritoshpandit2001@gmail.com>
2025-11-15 19:42:32 +0530

    Create 04_deliveries_v02.sql

---
+++ 04_deliveries_v02.sql
@@ -0,0 +1,8 @@
+CREATE TABLE deliveries_v02 AS
+SELECT *,
+       CASE
+           WHEN total_runs >= 4 THEN 'boundary'
+           WHEN total_runs = 0 THEN 'dot'
+           ELSE 'other'
+       END AS ball_result
+FROM deliveries;
